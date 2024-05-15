32dace2dd0971c03203b8f6d2daddd30c6c7032c
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-05-15 19:57:32 +0530

    Ignore Again

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Circle Server Map</title>
     <link rel="icon" type="image/png" href="india.png">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href='https://api.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.css' rel='stylesheet' />
     <link rel="stylesheet"
         href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.2/mapbox-gl-geocoder.css"
@@ -20,9 +20,9 @@
 <body>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script> -->
+        crossorigin="anonymous"></script>
     <script src='https://api.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.js'></script>
     <script
         src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.2/mapbox-gl-geocoder.min.js"></script>
@@ -37,8 +37,9 @@
     while ($data = mysqli_fetch_assoc($result)) {
         ?>
         <script type="text/javascript">
-            const marker<?php echo $data['Idx']; ?> = '<h3 style="color: #FF671F;"><?php echo $data['City']; ?></h3>'+
-                '<pre style="color: #046A38;"><?php echo 'Long: ' . $data['Lng'] . ', Lat: ' . $data['Lat']; ?></pre>';
+            const marker<?php echo $data['Idx']; ?> = '<p class="h2" style="color: #FF671F;"><?php echo $data['City']; ?></p>'+
+                '<pre style="color: #046A38;"><?php echo 'Long: ' . $data['Lng'] . ', Lat: ' . $data['Lat']; ?></pre>'+
+                '<button class="btn btn-primary btn-sm">Copy Lat/Long</button>';
             new mapboxgl.Marker({
                 color: '#FF671F',
                 draggable: false
