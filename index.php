16c19b9ab6bbbada753ddbd7c368d6e4014c5836
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-05-16 15:55:29 +0530

    .

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
-<na>
+<body>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
