1663380bda8768efc2542ea1e9055b5d5f28f9b0
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-05-22 12:58:07 +0530

    Minor Changes

--- index.php
+++ index.php
@@ -40,8 +40,12 @@
         '<path d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"/>'+
         '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5zm6.854 7.354-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708"/>'+
         '</svg>';
+
         <?php $query = "SELECT * FROM cities";
         $result = mysqli_query($db, $query);
+        $infoIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">'.
+        '<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>'.
+        '</svg>';
 
         while ($data = mysqli_fetch_assoc($result)) {
             ?>
@@ -56,7 +60,7 @@
             $query1 = "SELECT * FROM servers WHERE City = ('$city')";
             $result1 = mysqli_query($db, $query1);
 
-            if (mysqli_num_rows($result1) == 0) echo 'No Servers Installed at this Location.';
+            if (mysqli_num_rows($result1) == 0) echo $infoIcon . ' No Servers Installed at this Location.';
             else {
                 echo '<table><thead><tr><th>Mgmt IP</th><th>IPv6</th></tr></thead><tbody>';
                 while ($data1 = mysqli_fetch_assoc($result1)) {
@@ -65,6 +69,7 @@
                 echo '</tbody></table>';
             }
             // echo 'Long: ' . $data['Lng'] . ', Lat: ' . $data['Lat']; ?></div>';
+
             options<?php echo $data['Idx']; ?>.innerHTML = '<div class="btn-group btn-group-sm">'+
             '<button class="btn btn-outline-primary" id="lng<?php echo $data['Idx']; ?>" title="Copy Longitude">'+
             copyIcon+' Long</button>'+
