b89b283ccecb1dbc28c85dbe8b0a5b8107f6702f
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-05-27 19:30:32 +0530

    Disable Distance Feature

--- index.php
+++ index.php
@@ -30,11 +30,11 @@
     <!-- <script src="https://cdn.jsdelivr.net/npm/@mapbox-controls/ruler@1.2.0/src/index.min.js"></script> -->
     <!-- <script src="JS/colorToggle.js"></script> -->
     <div id="map"></div>
-    <div id="distance" class="distance-container"></div>
+    <!-- <div id="distance" class="distance-container"></div> -->
     <pre id="mouseCoord"></pre>
 
     <script src="JS/map.js"></script>
-    <script src="JS/ruler.js"></script>
+    <!-- <script src="JS/ruler.js"></script> -->
     <script type="text/javascript">
         const copyIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-fill" viewBox="0 0 16 16">'+
         '<path fill-rule="evenodd" d="M10 1.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5zm-5 0A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5v1A1.5 1.5 0 0 1 9.5 4h-3A1.5 1.5 0 0 1 5 2.5zm-2 0h1v1A2.5 2.5 0 0 0 6.5 5h3A2.5 2.5 0 0 0 12 2.5v-1h1a2 2 0 0 1 2 2V14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3.5a2 2 0 0 1 2-2"/>'+
