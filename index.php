c10a8e3bd6f265c97d4f14d382166d5481b03eda
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-06-03 15:48:26 +0530

    Minor Edit

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     <!-- <div id="distance" class="distance-container"></div> -->
     <div class="card position-absolute top-0 start-0 mt-2 ms-2" id="mapOptions">
         <div class="card-header">
-            <h6 class="card-title">Select Map Style</h6>
+            <h6 class="card-title">Map Style</h6>
             <select class="form-select" id="mapStyle">
                 <option value="mapbox://styles/mapbox/standard" selected>Standard</option>
                 <option value="mapbox://styles/mapbox/streets-v12">🛣️ Streets</option>
@@ -48,8 +48,8 @@
         </div>
         <div class="card-footer">
             <span class="d-flex align-items-baseline">
-                <h6 class="card-title me-auto">Select Light Preset</h6>
-                <attr title="Below Sections are ONLY Valid for 'Standard' Map Style.">
+                <h6 class="card-title me-auto">Light Preset</h6>
+                <attr title="ONLY for 'Standard' Map Style.">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247m2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/>
                     </svg>
@@ -63,24 +63,31 @@
             </select>
         </div>
         <div class="card-footer">
-            <h6 class="dropdown-toggle" id="mapLabelsToggle" role="button" data-bs-toggle="collapse" data-bs-target="#mapLabels">Toggle Map Legend</h6>
+            <span class="d-flex align-items-baseline">
+                <h6 class="card-title dropdown-toggle me-auto" id="mapLabelsToggle" role="button" data-bs-toggle="collapse" data-bs-target="#mapLabels">Map Legend</h6>
+                <attr title="ONLY for 'Standard' Map Style.">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247m2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/>
+                    </svg>
+                </attr>
+            </span>
         </div>
         <ul class="collapse list-group list-group-flush" id="mapLabels">
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showPlaceLabels" checked>
-                <label for="showPlaceLabels">Place Labels</label>
+                <label class="stretched-link" for="showPlaceLabels">Place Labels</label>
             </li>
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showPointOfInterestLabels" checked>
-                <label for="showPointOfInterestLabels">POI Labels</label>
+                <label class="stretched-link" for="showPointOfInterestLabels">POI Labels</label>
             </li>
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showRoadLabels" checked>
-                <label for="showRoadLabels">Road Labels</label>
+                <label class="stretched-link" for="showRoadLabels">Road Labels</label>
             </li>
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showTransitLabels" checked>
-                <label for="showTransitLabels">Transit Labels</label>
+                <label class="stretched-link" for="showTransitLabels">Transit Labels</label>
             </li>
         </ul>
     </div>
