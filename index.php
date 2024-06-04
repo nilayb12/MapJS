266e7d772e3108c80f9b6b7fac3fb51b1d32cf16
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-06-04 12:45:34 +0530

    Map Legend Toggle

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
         </div>
         <div class="card-footer">
             <span class="d-flex align-items-baseline">
-                <h6 class="card-title dropdown-toggle me-auto" id="mapLabelsToggle" role="button" data-bs-toggle="collapse" data-bs-target="#mapLabels">Map Legend</h6>
+                <h6 class="card-title dropdown-toggle me-auto" id="mapLabelsToggle" role="button" data-bs-toggle="collapse" data-bs-target="#mapLabels">Map Labels</h6>
                 <attr data-bs-toggle="tooltip" data-bs-title="ONLY for 'Standard' Map Style.">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247m2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/>
@@ -75,35 +75,44 @@
         <ul class="collapse list-group list-group-flush" id="mapLabels">
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showPlaceLabels" checked>
-                <label class="stretched-link" for="showPlaceLabels">Place Labels</label>
+                <label class="stretched-link" for="showPlaceLabels">🏢 Place Labels</label>
             </li>
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showPointOfInterestLabels" checked>
-                <label class="stretched-link" for="showPointOfInterestLabels">POI Labels</label>
+                <label class="stretched-link" for="showPointOfInterestLabels">🏟️ POI Labels</label>
             </li>
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showRoadLabels" checked>
-                <label class="stretched-link" for="showRoadLabels">Road Labels</label>
+                <label class="stretched-link" for="showRoadLabels">🛣️ Road Labels</label>
             </li>
             <li class="list-group-item">
                 <input class="form-check-input" type="checkbox" id="showTransitLabels" checked>
-                <label class="stretched-link" for="showTransitLabels">Transit Labels</label>
+                <label class="stretched-link" for="showTransitLabels">🚉 Transit Labels</label>
             </li>
         </ul>
     </div>
     <div class="card position-absolute start-0 ms-1 z-1" id="mapLegend" style="bottom: 5rem;">
         <div class="card-header">
-            <h6 class="card-title">Legend</h6>
+            <h6 class="card-title">Map Legend</h6>
         </div>
-        <div class="card-footer">
-            <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF671F" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"></path>
-            </svg> Servers Installed</span>
-            <br>
-            <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0098e0" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"></path>
-            </svg> No Servers</span>
-        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <input class="form-check-input" type="checkbox" id="serversPrs" checked>
+                <label  class="stretched-link" for="serversPrs">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF671F" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"></path>
+                    </svg> Servers Installed
+                </label>
+            </li>
+            <li class="list-group-item">
+                <input class="form-check-input" type="checkbox" id="serversAbs" checked>
+                <label class="stretched-link" for="serversAbs">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0098e0" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"></path>
+                    </svg> No Servers
+                </label>
+            </li>
+        </ul>
     </div>
     </div>
     <pre id="mouseCoord"></pre>
@@ -197,6 +206,21 @@
             <?php
         }
         ?>
+
+        document.getElementById('serversPrs').addEventListener('change', function () {
+            document.querySelectorAll('.mapboxgl-marker').forEach((marker) => {
+                if(marker.children[0].children[2].getAttribute('fill')=='#FF671F'){
+                    this.checked==false?marker.style.setProperty('visibility', 'hidden'):marker.style.removeProperty('visibility');
+                }
+            });
+        });
+        document.getElementById('serversAbs').addEventListener('change', function () {
+            document.querySelectorAll('.mapboxgl-marker').forEach((marker) => {
+                if(marker.children[0].children[2].getAttribute('fill')=='#0098e0'){
+                    this.checked==false?marker.style.setProperty('visibility', 'hidden'):marker.style.removeProperty('visibility');
+                }
+            });
+        });
     </script>
 </body>
 
