0ca3f06cd7ce662936a5168b9fe4fdeacc01aab8
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-07-12 16:43:35 +0530

    Ignore

--- modules/mapOptions.php
+++ modules/mapOptions.php
@@ -17,7 +17,7 @@
         <span class="d-flex">
             <h6 class="card-title me-auto">Light Preset</h6>
             <attr data-bs-toggle="tooltip" data-bs-html="true"
-                data-bs-title="This Option is Limited to the <code>STANDARD</code> Map Style.">
+                data-bs-title="This Option is Limited to the <code><strong>STANDARD</strong></code> Map Style.">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                     class="bi bi-question-circle-fill" viewBox="0 0 16 16">
                     <path
@@ -41,7 +41,7 @@
                         d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
                 </svg></h6>
             <attr data-bs-toggle="tooltip" data-bs-html="true"
-                data-bs-title="This Option is Limited to the <code>STANDARD</code> Map Style.">
+                data-bs-title="This Option is Limited to the <code><strong>STANDARD</strong></code> Map Style.">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                     class="bi bi-question-circle-fill" viewBox="0 0 16 16">
                     <path
