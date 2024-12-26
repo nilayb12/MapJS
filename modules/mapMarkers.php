d564766b9da7558fb83e63b8f689adbfa3c4c263
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-12-26 19:41:33 +0530

    Ignore

--- modules/mapMarkers.php
+++ modules/mapMarkers.php
@@ -9,7 +9,7 @@
 
     <?php $query = "SELECT * FROM cities";
     $result = mysqli_query($db, $query);
-    $infoIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">' .
+    $infoIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill mb-1" viewBox="0 0 16 16">' .
         '<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>' .
         '</svg>';
 
@@ -40,7 +40,7 @@
             if (mysqli_num_rows($result1) == 0)
                 echo '<div class="card-body" style="color: brown;">' . $infoIcon . ' No Servers Installed at this Location.' . '</div>';
             else {
-                echo '<table class="card-body table table-sm table-bordered table-striped table-hover table-group-divider">' .
+                echo '<table class="card-body table table-sm table-bordered table-striped table-hover table-group-divider mb-0">' .
                     '<caption>' . $infoIcon . ' Click an IP to Copy its Value.</caption>' .
                     '<thead><tr><th>Server</th><!--<th>Mgmt IP</th>--><th>IPv6</th></tr></thead><tbody class="table-group-divider">';
                 while ($data1 = mysqli_fetch_assoc($result1)) {
